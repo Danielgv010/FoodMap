cef6487e5fd27d85ca6cd1d2539f65a2e52df9fa
Danielgv010 <danielgarciav010@gmail.com>
2025-02-24 14:13:59 +0100

    Renamed API

--- load_document/urls.py
+++ load_document/urls.py
@@ -4,5 +4,5 @@ from .views import OCRFileUploadView, TextToJsonView, OcrToJsonView
 urlpatterns = [
     path('upload-pdf/', OCRFileUploadView.as_view(), name='upload-pdf'),
     path('text-to-json/', TextToJsonView.as_view(), name='text-to-json'),
-    path('pdf_ocr_to_json/', OcrToJsonView.as_view(), name='pdf_ocr_to_json'),
+    path('ocr_to_json/', OcrToJsonView.as_view(), name='ocr_to_json'),
 ]
