7eb5b2e863908112c2f4e221f54020068e9a8e37
Danielgv010 <danielgarciav010@gmail.com>
2025-02-26 11:29:48 +0100

    Renamed register to accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.RegisterView.as_view(), name='register'),
-    path('submit/', views.RegisterAPIView.as_view(), name='register-submit'),
+    path('register/', views.RegisterAPIView.as_view(), name='register-submit'),
 ]
