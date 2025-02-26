59e88bb3e78974d2f9b9c9cbc88308fc3d684d94
Danielgv010 <danielgarciav010@gmail.com>
2025-02-26 09:21:37 +0100

    Register front connected to back

--- accounts/static/accounts/js/register.js
+++ accounts/static/accounts/js/register.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Accordion functionality
     const deployTrigger = document.getElementById('deployTrigger');
     const locationGroup = document.getElementById('location-group');
     const triangle = document.querySelector('.triangle');
-    const locationInput = locationGroup.querySelector('input'); // Get the input inside location-group
+    const locationInput = locationGroup.querySelector('input');
 
     deployTrigger.addEventListener('click', function() {
         locationGroup.classList.toggle('visible');
         triangle.classList.toggle('rotated');
     });
 
-    // Check input on page load for label styling
     function checkInputValidity(inputElement) {
         if (inputElement.value) {
             inputElement.classList.add('valid');
@@ -19,17 +17,15 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    // Apply initial check and listen for input events to update the label
     const inputs = document.querySelectorAll('.form-input-group input');
     inputs.forEach(input => {
-        checkInputValidity(input); // Check on page load
+        checkInputValidity(input);
 
         input.addEventListener('input', () => {
             checkInputValidity(input);
         });
     });
 
-    //Fix for location input
     if (locationInput) {
         locationInput.addEventListener('input', function() {
             if (this.value) {
@@ -39,9 +35,64 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
 
-        // Initial check in case the input has a value on page load
         if (locationInput.value) {
             locationInput.setAttribute('valid', true);
         }
+    }
+
+    const form = document.getElementById('registrationForm');
+    const apiUrl = form.dataset.url;
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+
+        fetch(apiUrl, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'X-CSRFToken': getCookie('csrftoken')
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(err => { throw err; });
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle success
+            window.location.href = "/"
+
+        })
+        .catch(error => {
+            // Handle errors
+            if (error && typeof error === 'object') {
+              if(error.detail){
+                alert(JSON.stringify(error.detail));
+              } else {
+                alert(JSON.stringify(error));
+              }
+
+            } else {
+                alert('An error occurred: ' + error);
+            }
+        });
+    });
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
     }
 });
